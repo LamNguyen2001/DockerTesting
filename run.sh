caddb820d068e360884b0f8341060bb6c9ee5085
LamNguyen2001 <70511520+LamNguyen2001@users.noreply.github.com>
2023-04-27 10:00:04 +0700

    Add files via upload

--- run.sh
+++ run.sh
@@ -27,11 +27,11 @@ sudo systemctl start docker
 
 # build image for docker
 echo "build the image file"
-docker build -t myfirstimage:test1 -f DockerFile .
+docker build -t hello -f dockerfile .
 
 # create container from image on port 9876
 echo "container is running on port 9876"
 echo "connect using http://<ip address>:9876/the_converter.html"
 
 #docker run -it --name mywebserver -p 9876:80 -h firstserver myfirstimage:test1
-docker run --name mywebserver -p 9876:80 -v /root/DockerTesting:/var/www/html -it myfirstimage:test1 -D FOREGROUND
+docker run --name ab -p 9876:80 -v /root/DockerTesting:/var/www/html -it hello -D FOREGROUND
